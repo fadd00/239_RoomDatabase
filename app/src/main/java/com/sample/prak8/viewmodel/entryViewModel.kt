3cf9e2b9ab1c06aaa37d46bb1540e34b6159f1ea
fadd00 <andhikahutama9@gmail.com>
2025-11-27 13:05:03 +0700

    feat: add `toDetailSiswa` extension function for `Siswa` object mapping

--- app/src/main/java/com/sample/prak8/viewmodel/entryViewModel.kt
+++ app/src/main/java/com/sample/prak8/viewmodel/entryViewModel.kt
@@ -44,3 +44,10 @@ fun Siswa.toUiStateSiswa(isEntryValid: Boolean = false): UIStateSiswa = UIStateS
     detailSiswa = this.toDetailSiswa(),
     isEntryValid = isEntryValid
 )
+
+fun Siswa.toDetailSiswa(): DetailSiswa = DetailSiswa(
+    id = id,
+    nama = nama,
+    alamat = alamat,
+    telpon = telpon
+)
